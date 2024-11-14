f654a28edc659da59d6f93cd10addb40bbbf1dc8
lordbeerusza <Ntaliep0@gmail.com>
2024-11-14 16:37:23 +0200

    hello world

--- myApp.js
+++ myApp.js
@@ -1,41 +1,18 @@
 let express = require('express');
 let app = express();
+
+// Log to the console
 console.log("Hello World");
 
+// Define a basic route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
